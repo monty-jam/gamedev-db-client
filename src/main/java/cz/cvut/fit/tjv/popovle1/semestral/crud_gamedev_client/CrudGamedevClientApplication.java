2632c6cdb09d34a244a9403c47f78c36e73256cb
popovle1 <popovle1@fit.cvut.cz>
2022-01-13 21:16:17 +0100

    Initial commit

---
+++ src/main/java/cz/cvut/fit/tjv/popovle1/semestral/crud_gamedev_client/CrudGamedevClientApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.popovle1.semestral.crud_gamedev_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudGamedevClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudGamedevClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/cvut/fit/tjv/popovle1/semestral/crud_gamedev_client/CrudGamedevClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.popovle1.semestral.crud_gamedev_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudGamedevClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
