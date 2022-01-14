113205584be8c8b67e137f22c9f198cab2eda73f
popovle1 <popovle1@fit.cvut.cz>
2022-01-14 19:29:16 +0100

    Fully implemented DevClient.

--- src/main/java/cz/cvut/fit/tjv/popovle1/semestral/crud_gamedev_client/data/DevClient.java
+++ src/main/java/cz/cvut/fit/tjv/popovle1/semestral/crud_gamedev_client/data/DevClient.java
@@ -1,5 +1,96 @@
 package cz.cvut.fit.tjv.popovle1.semestral.crud_gamedev_client.data;
 
-public class DevClient {
+import cz.cvut.fit.tjv.popovle1.semestral.crud_gamedev_client.dto.DevDTO;
+import cz.cvut.fit.tjv.popovle1.semestral.crud_gamedev_client.ui.DevView;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
 
+import java.time.Duration;
+import java.util.Collection;
+
+@Component
+public class DevClient {
+    private final WebClient devWebClient;
+    private final DevView devView;
+    private Long currentId;
+
+    public DevClient(@Value("${backend_url}") String backedUrl, DevView devView) {
+        devWebClient = WebClient.create(backedUrl + "/devs");
+        this.devView = devView;
+    }
+
+    public Long getCurrentId() {
+        return currentId;
+    }
+
+    public void setCurrentId(Long currentId) {
+        this.currentId = currentId;
+        if(currentId != null)
+            try {
+                read();
+            } catch (WebClientException e) {
+                this.currentId = null;
+                throw e;
+            }
+    }
+
+    public DevDTO create(DevDTO dev) {
+        return devWebClient.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(dev)
+                .retrieve()
+                .bodyToMono(DevDTO.class)
+                .block(Duration.ofSeconds(5));
+    }
+
+    public Collection<DevDTO> readAll() {
+        return devWebClient.get()
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(DevDTO.class)
+                .collectList()
+                .block();
+    }
+
+    public DevDTO read() {
+        if(currentId == null)
+            throw new IllegalStateException("Current Id not set.");
+        return devWebClient.get()
+                .uri("/{id}", currentId)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(DevDTO.class)
+                .block();
+    }
+
+    public void update(DevDTO dev) {
+        if(currentId == null)
+            throw new IllegalStateException("Current id is not set.");
+        dev.setId(currentId);
+        devWebClient.put()
+                .uri("/{id}", currentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(dev)
+                .retrieve()
+                .toBodilessEntity()
+                .subscribe(x -> {}, e -> {devView.printErrorUpdate(e);});
+    }
+
+    public void delete() {
+        if(currentId == null)
+            throw new IllegalStateException("Current id is not set.");
+        devWebClient.delete()
+                .uri("/{id}", currentId)
+                .retrieve()
+                .toBodilessEntity()
+                .subscribe(
+                        x -> {setCurrentId(null);},
+                        e -> {devView.printErrorDev(e);}
+                );
+    }
 }
--- src/main/java/cz/cvut/fit/tjv/popovle1/semestral/crud_gamedev_client/dto/DevDTO.java
+++ src/main/java/cz/cvut/fit/tjv/popovle1/semestral/crud_gamedev_client/dto/DevDTO.java
@@ -1,17 +1,21 @@
 package cz.cvut.fit.tjv.popovle1.semestral.crud_gamedev_client.dto;
 
 public class DevDTO {
-    private final String name;
-    private final String surname;
-    private final String specialization;
-    private final Long studioId;
+    private Long id;
+    private String name;
+    private String surname;
+    private String specialization;
+    private Long studioId;
 
-    public DevDTO(String name, String surname, String specialization, Long studioId) {
+    public DevDTO(Long id, String name, String surname, String specialization, Long studioId) {
+        this.id = id;
         this.name = name;
         this.surname = surname;
         this.specialization = specialization;
         this.studioId = studioId;
     }
+
+    public Long getId() {return id;}
 
     public String getName() {
         return name;
@@ -27,5 +31,25 @@ public class DevDTO {
 
     public Long getStudioId() {
         return studioId;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public void setStudioId(Long studioId) {
+        this.studioId = studioId;
     }
 }
